a9c17d6c12495235d27c3343bbd2764acfad59a9
r.lassen <lassenrafaela@gmail.com>
2022-03-06 21:42:42 -0300

    first commit

---
+++ pages/Exemplo.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <p>Olá {{nome}}</p>
+        {{dia}}
+        <v-btn color="blue" height="100" @click="trocarNome('vitor')">Trocar nome</v-btn>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        nome: 'Rafinha'
+    }), 
+    methods: {
+        trocarNome(nome){
+            this.nome = this.definirNome(nome)
+        },
+        definirNome(nome){
+            return nome;
+        }
+    },
+    computed:{
+        dia(){
+            return 3
+
+        }
+    },
+    watch:{
+        nome(newvalue, velho){
+            console.log(newvalue)
+            console.log(velho)
+        }
+    },
+    created(){
+        this.nome = "Joao"
+    }
+
+}
+</script>
+
+<style>
+
+</style>
